0461e0736b8e603c826f095bdbdaa130b8a9e811
Pavlo Starobub <amorallex@gmail.com>
2026-06-20 22:56:09 +0300

    add post queries

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
 
 
 @app.get("/")
@@ -23,6 +29,12 @@ async def read_root() -> dict:
 # async def greet_name(name: str, age: int) -> dict:
 #     return {"message": f"Hello {name}, age: {age}"}
 
+
 @app.get("/greet/")
 async def greet_name(age: int = 0, name: str | None = "User") -> dict:
     return {"message": f"Hello {name}, age: {age}"}
+
+
+@app.post("/create_book/")
+async def create_book(book_data: BookCreateModel) -> dict:
+    return {"message": f"Book created: {book_data.title} by {book_data.author}"}
